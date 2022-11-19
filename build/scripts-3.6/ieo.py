e0ecdc3ff35949ca1c450bd033995c8facca09fe
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-19 04:30:46 +0000

    Adding tarfile member sanitization to extractall()

--- build/scripts-3.6/ieo.py
+++ build/scripts-3.6/ieo.py
@@ -755,7 +755,26 @@ def untarfile(file, outdir):
     print('Extracting {} to: {}'.format(basename, outpath))
     try:
         with tarfile.open(file) as tar:
-            tar.extractall()
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar)
         filelist = glob.glob(os.path.join(outpath, '*.*'))
         # tar.close()
         if os.path.isdir(os.path.join(outpath, 'gap_mask')):
